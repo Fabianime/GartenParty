0fdfeccbb4a12f9851ded66050b001c2414ee4a8
Fabian Bedarf <fabian.bedarf@live.de>
2018-07-11 17:30:50 +0200

    Did some cleanup. Better Lgoin

--- gartenPartyFrontend/src/app/widgets/login/login.component.ts
+++ gartenPartyFrontend/src/app/widgets/login/login.component.ts
@@ -10,32 +10,22 @@ import {LoginService} from '../../services/login.service';
 export class LoginComponent {
 
   public gartenPartyId;
-  private _bLogin;
+  public loginInvalide;
 
   @Output() bLoginChange = new EventEmitter();
-
-  @Input()
-  get bLogin() {
-    return this._bLogin;
-  }
-
-  set bLogin(val) {
-    this._bLogin = val;
-    this.bLoginChange.emit(this._bLogin);
-  }
 
   constructor(private loginService: LoginService) {
   }
 
-  public setGartenPartyID() {
+  public checkLogin() {
     this.loginService.setGartenPartyID(this.gartenPartyId);
-    this.checkLogin();
-  }
-
-  private checkLogin() {
     this.loginService.checkGartenPartyID().subscribe((data) => {
       if (data.status === 200) {
-        this.bLogin = data.response;
+        if (data.response === true) {
+          location.reload();
+        } else {
+          this.loginInvalide = true;
+        }
       } else {
         throw data.error;
         // toDO: return False + logs mit fehler schreiben
